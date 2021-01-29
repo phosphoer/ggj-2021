3f2cbc135a33ceb676935ff01dec9daeabd5c61a
Brendan Walker <brendan@millerwalker.net>
2021-01-29 09:34:01 -0800

    Porting over game classes from 2020

---
+++ ggj-2021/Assets/Scripts/UI/UIViewportScaler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class UIViewportScaler : MonoBehaviour
+{
+  [SerializeField]
+  private Vector2 _referenceResolution = new Vector2(1280, 800);
+
+  private Vector2 _lastScale;
+  private CanvasScaler _canvasScaler;
+
+  private void Start()
+  {
+    _canvasScaler = GetComponent<CanvasScaler>();
+  }
+
+  private void Update()
+  {
+    if (_canvasScaler != null)
+    {
+      float scaleX = Screen.width / _referenceResolution.x;
+      float scaleY = Screen.height / _referenceResolution.y;
+      if (scaleX == 0 || scaleY == 0 || (scaleX == _lastScale.x && scaleY == _lastScale.y))
+        return;
+
+      _canvasScaler.scaleFactor = Mathf.Min(scaleX, scaleY);
+      _lastScale = new Vector2(scaleX, scaleY);
+    }
+  }
+
+  [ContextMenu("Apply Scale")]
+  private void ApplyScale()
+  {
+    Debug.LogFormat("Applying scale for screen res {0}x{1}", Screen.width, Screen.height);
+    if (_canvasScaler == null)
+    {
+      _canvasScaler = GetComponent<CanvasScaler>();
+    }
+
+    Update();
+  }
+}
---
+++ ggj-2021/Assets/Scripts/UI/WinGameUIHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGameUIHandler : MonoBehaviour
+{
+  public void OnPlayAgainClicked()
+  {
+    SceneManager.LoadScene("GameScene");
+  }
+
+  public void OnQuitGameClicked()
+  {
+    //If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+    //Quit the application
+    Application.Quit();
+#endif
+
+    //If we are running in the editor
+#if UNITY_EDITOR
+    //Stop playing the scene
+    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+  }
+}
