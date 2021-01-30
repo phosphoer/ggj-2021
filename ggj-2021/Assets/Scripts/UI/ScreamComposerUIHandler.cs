5403c9c7c386a484af71b4dc32d21939fa044163
Brendan Walker <brendan@millerwalker.net>
2021-01-29 23:29:45 -0800

    Waypoint check in for scream composer

---
+++ ggj-2021/Assets/Scripts/UI/ScreamComposerUIHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreamComposerUIHandler : MonoBehaviour
+{
+  [SerializeField]
+  private Button[] _screamTrack;
+
+  [SerializeField]
+  private RMF_RadialMenu _radialMenu = null;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
